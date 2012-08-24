94e4a1df4ca0efa3427d70aa760ca0a3218134b3
thiyagarajan <thiyagarajannv@yahoo.com>
2012-08-24 12:13:51 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Selenium::Application.config.secret_token = 'c6c99ceb250efdae3d959977143653bc153b3e5dc24a7d6921bcbc3dda013c267a7915807309540474ac7dc0793da1bf21195d419253216c2504f710b99dcd49'
